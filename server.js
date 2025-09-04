fabb1f51d851a84b0e233a54af59616dfbebad9c
salambaee <salamrizki613@gmail.com>
2025-09-04 16:06:08 +0700

    server.js

--- server.js
+++ server.js
@@ -7,17 +7,71 @@ let movie = [
     { id: 2, title: "Anime", director: "Salam", year: "1999"}
 ];
 
+let directors = [
+    { id: 1, name: "Salam Rizqi Mulia", birthYear: "2005"},
+    { id: 2, name: "Andi Frimawan", birthYear: "2001"}
+];
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Selamat datang di Rest API pertamamu!');
 });
 
+// Mengembalikan daftar semua sutradara
+app.get('/directors', (req, res) => {
+    res.json(directors);
+});
+
+// Mengembalikan detail satu sutradata
+app.get('/directors/:id', (req, res) => {
+    const getDirectories = directors.find(d => d.id === parseInt(req.params.id));
+    if (getDirectories) {
+        res.json(getDirectories);
+    } else {
+        res.status(404).json({ message: 'Directories not found'});
+    }
+});
+
+// Membuat seorang sutradara baru. Lakukan validasi untuk memastikan name dan birthYear disertakan.
+app.post('/directors', (req, res) => {
+    const { name, birthYear} = req.body || {};
+    if (!name || !birthYear) {
+        return res.status(400).json({ error: `name, birthYear (wajib diisi)`});
+    }
+    const newDirectors = { id: movie.length + 1, name, birthYear };
+    movie.push(newDirectors);
+    res.status(201).json(newDirectors);
+});
+
+// Memperbarui data sutradata
+app.put('/directors/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const directorIndex = directors.findIndex(m => m.id === id);
+    if (directorIndex === -1) {
+        return res.status(404).json({  error: `Sutradara tidak ditemukan`});
+    }
+    const { name, birthYear } = req.body || {};
+    const updateDirector = { name, birthYear };
+    directors[directorIndex] = updateDirector;
+    res.json(updateDirector);
+});
+
+// Mengapus data sutradara
+app.delete('/directors/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const directorIndex = directors.findIndex(m => m.id === id);
+    if (directorIndex === -1) {
+        return res.status(404).json({ error: `Diretor tidak ditemukan` });
+    }
+    directors.splice(directorIndex, 1);
+    res.status(204).send();
+});
+
 app.get('/movie', (req, res) => {
     res.json(movie);
 });
 
-// Get data by ID
 app.get('/movie/:id', (req, res) => {
     const getMovie = movie.find(m => m.id === parseInt(req.params.id));
     if (getMovie) {
@@ -27,7 +81,7 @@ app.get('/movie/:id', (req, res) => {
     }
 });
 
-// Add data
+// POST - Membuat film baru
 app.post('/movie', (req, res) => {
     const { title, director, year } = req.body || {};
     if (!title || !director || !year) {
@@ -38,8 +92,21 @@ app.post('/movie', (req, res) => {
     res.status(201).json(newMovie);
 });
 
-// Hapus data
-app.delete('/movies/:id', (req, res) => {
+// PUT - Memperbarui data film
+app.put('/movie/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const movieIndex = movie.findIndex(m => m.id === id);
+    if (movieIndex === -1) {
+        return res.status(404).json({  error: `Movie tidak ditemukan`});
+    }
+    const { title, director, year } = req.body || {};
+    const updateMovie = { id, title, director, year };
+    movie[movieIndex] = updateMovie;
+    res.json(updateMovie);
+});
+
+// DELETE - Mengapus film
+app.delete('/movie/:id', (req, res) => {
     const id = Number(req.params.id);
     const movieIndex = movie.findIndex(m => m.id === id);
     if (movieIndex === -1) {
